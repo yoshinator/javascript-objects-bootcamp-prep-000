4477aa5c1fd6da1ca5811c640b93cf0d68082676
yoan ante <yoanante@gmail.com>
2018-08-30 18:24:07 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = new Object({journey: "don't stop believing"});
 
 function updatePlaylist(list, artist, songTitle){
-list.artist = songTitle;
+list[artist] = songTitle;
 return list;
 }
